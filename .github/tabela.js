ad2adffea7015bac1f8a2f3225bd1c49f8e5b899
Maiara <mayaramellovon@yahoo.com.br>
2021-08-13 11:34:46 -0400

    Tabela de Multiplicação

--- .github/tabela.js
+++ .github/tabela.js
@@ -1,9 +1,9 @@
 function tabelaMultiplicacao(x) {
     let resultado = []
-    for (let linha = 0; linha <= x; linha++) {
+    for (let linha = 0; linha <= x; linha++) {        //linha da tabela
         resultado[linha] = [];
-        for(let coluna = 0; coluna <= x; coluna++){
-            resultado[linha][coluna]= linha*coluna
+        for(let coluna = 0; coluna <= x; coluna++){   // coluna da tabela
+            resultado[linha][coluna]= linha*coluna    
         }
     }
     console.table(resultado)
